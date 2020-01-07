71676489656de9075efad0f7b3ae40e2634ec985
Carter Trybulski <carter.trybulski@kearsarge.org>
2020-01-07 10:35:41 -0500

    Hud now reads and writes to files to save game

--- Hud.py
+++ Hud.py
@@ -2,13 +2,60 @@ import pygame, sys, math
 
 class Hud():
     def __init__(self, baseText, startPos=[0,1]):
+        if self.kind == "score":
+            self.baseText = "Score: "
+            self.file = "saves/scorecounter.txt"
+        if self.kind == "death":
+            self.baseText = "Deaths: "
+            self.file = "saves/deathcounter.txt"
         pygame.font.get_fonts()
         pygame.font.match_font
         self.font = pygame.font.Font(None, 50)
         self.baseText = baseText
         self.image = self.font.render("Score: 0", 1, (0, 0, 0))
         self.rect = self.image.get_rect(topleft = startPos)
+    
     def update(self, score):
         text = self.baseText + str(score)
         self.image = self.font.render(text , 1, (0, 0, 0))
         self.rect = self.image.get_rect(topleft = self.rect.topleft)
+
+    def die(self):
+        in_file = open(self.file, "r")
+        count = int(in_file.read())
+        in_file.close()
+        count = count + 1
+        print(count)
+        out_file = open(self.file, "w")
+        out_file.write(str(deathcount))
+        out_file.close()
+
+if __name__ == "__main__":
+    # Death Counter
+
+    # Variables
+    deathcount = float(0)
+
+    pygame.init()
+    BLACK = (0, 0, 0)
+    WIDTH = 320
+    HEIGHT = 260
+    windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+
+    windowSurface.fill(BLACK)
+    
+    hud = Hud("Deaths")
+
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    hud.die()
+                if event.key == pygame.K_2:
+                    deathcount = 0
+                    with open("deathcounter.txt", "w") as out_file:
+                        out_file.write(str(deathcount))
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
