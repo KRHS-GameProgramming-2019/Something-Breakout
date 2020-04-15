cbba2a8b84b4a9303b0e902d0a1f435799b770ec
cspooner <cspooner@kearsarge.org>
2020-04-15 14:36:35 -0400

    Work on piece loader

--- Pieces.py
+++ Pieces.py
@@ -20,42 +20,75 @@ class Pieces():
             piece=[]
             curKind=None
             for blockNum, block in enumerate(line):
+                print("a. " + str(block))
                 if not curKind:         #start of line
+                    print(" b. No currKind")
                     if lineNum == 0:    #first block
+                        print("  c. First Block")
                         curKind = block.kind
                         piece += [block]
                         block.piece = piece
-                    else:
-                        if self.blocks[lineNum - 1][blockNum].kind == block.kind:
+                    else:               #first block of new line
+                        #the one above us is like us
+                        print("  c. Not First Block")
+                        if self.blocks[lineNum - 1][blockNum].kind == block.kind:  
+                            print("   d. Match Above")
                             self.blocks[lineNum - 1][blockNum].piece += [block]
                             block.piece = self.blocks[lineNum - 1][blockNum].piece
                             piece = block.piece
                             curKind = block.kind
-                        else:
+                        else:       #the one above us is NOT like us
+                            print("   d. No Match Above")
                             curKind = block.kind
                             piece += [block]
                             block.piece = piece
-                elif curKind == block.kind:
-                    if lineNum == 0: 
+                elif curKind == block.kind:     #the one to the left is like us
+                    print(" b. currKind: " + curKind)
+                    if lineNum == 0: #top line special case
+                        print("  c. Top Row")
+                        piece += [block]
+                        block.piece = piece
+                    else:  # the rest of the lines
+                        # the one above us is like us
+                        print("  c. Not Top Row")
+                        if self.blocks[lineNum - 1][blockNum].kind == block.kind:
+                            print("   d. Match Above")
+                            self.blocks[lineNum - 1][blockNum].piece += [block]
+                            block.piece = self.blocks[lineNum - 1][blockNum].piece
+                            piece = block.piece
+                            curKind = block.kind
+                        else:       #the one above us is NOT like us
+                            print("   d. No Match Above")
+                            curKind = block.kind
+                            piece += [block]
+                            block.piece = piece
+                            
+                else: # the one to the left is NOT like us
+                    print(" b. kinds: " + curKind + " vs. " + block.kind)
+                    if piece not in self.pieces: #Store the previous piece pieces
+                        print("-------Break " + piece[0].kind + " Piece Off------")
+                        self.pieces += [piece]
+                    piece = []
+                    if lineNum == 0: #top line special case
+                        print("  c. Top Row")
+                        curKind = block.kind
                         piece += [block]
                         block.piece = piece
                     else: 
-                        if self.blocks[lineNum - 1][blockNum].kind == block.kind:
+                        print("  c. Not Top Row")
+                        #the one above us is like us
+                        if self.blocks[lineNum - 1][blockNum].kind == block.kind:  
+                            print("   d. Match Above")
                             self.blocks[lineNum - 1][blockNum].piece += [block]
                             block.piece = self.blocks[lineNum - 1][blockNum].piece
                             piece = block.piece
                             curKind = block.kind
-                        else:
+                        else:       #the one above us is NOT like us
+                            print("   d. No Match Above")
+                            curKind = block.kind
                             piece += [block]
                             block.piece = piece
-                else:
-                    if piece not in self.pieces:
-                        self.pieces += [piece]
-                    piece = []
-                    if lineNum == 0:
-                        curKind = block.kind
-                        piece += [block]
-                        block.piece = piece
+                        
             if piece not in self.pieces:
                 self.pieces += [piece]
         print (str(self))
